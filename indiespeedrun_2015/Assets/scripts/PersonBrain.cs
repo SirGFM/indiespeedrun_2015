5e52fc92790e51a1291a89ea1b645de3860378f2
SirGFM <gfmandaji@hotmail.com>
2015-10-24 15:12:51 -0200

    It's alive (i.e., the person can move)

--- indiespeedrun_2015/Assets/scripts/PersonBrain.cs
+++ indiespeedrun_2015/Assets/scripts/PersonBrain.cs
@@ -4,8 +4,7 @@ using System.Collections;
 public class PersonBrain : MonoBehaviour {
 
     /** Definitions of persons' types */
-    public enum enType
-    {
+    public enum enType {
         level_0 = 0,
         level_1,
         level_2,
@@ -13,8 +12,7 @@ public class PersonBrain : MonoBehaviour {
     };
 
     /** Definitions of persons' states relative to the player */
-    public enum enState
-    {
+    public enum enState {
         free = 0,
         bribed,
         persuaded,
@@ -38,29 +36,45 @@ public class PersonBrain : MonoBehaviour {
     public float minWalkTime = 2.0f;
     /** Maximum time the person will walk */
     public float maxWalkTime = 5.0f;
+    /** Leftmost position the person can go to */
+    public float minHorPosition = -10.0f;
+    /** Rightmost position the person can go to */
+    public float maxHorPosition = 10.0f;
+    /** The horizontal speed */
+    public float horizontalSpeed = 3.5f;
+
     /** Current state of the person, relative to the player */
     public enState state;
     /** Which of the classes the instance represent */
     public enType type;
     /** This object's color */
     public Color color;
+
     /** Current state of the AI */
     private enAIState aiState;
     /** Probably unneeded, but avoids calling the coroutine if it's already running */
     private bool isRunningAI;
     /** The currently running coroutine (duh!) */
     private Coroutine runningCoroutine = null;
+    /** The rigid body */
+    private Rigidbody2D rbody;
 
-	// Use this for initialization
-	void Start () {
-
+    // Use this for initialization
+    void Start() {
+        this.rbody = GetComponent<Rigidbody2D>();
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update() {
         // TODO Set the animation according with the state
 
-        // TODO Check if trying to go out of borders and stop (i.e., force idle)
+        // Forcibly reverse the velocity, if going out of bounds
+        if (this.transform.position.x <= this.minHorPosition) {
+            this.rbody.velocity = new Vector2(this.horizontalSpeed, 0.0f);
+        }
+        else if (this.transform.position.x >= this.maxHorPosition) {
+            this.rbody.velocity = new Vector2(-this.horizontalSpeed, 0.0f);
+        }
 
         if (!isRunningAI) {
             this.runningCoroutine = StartCoroutine(doAI());
@@ -72,6 +86,8 @@ public class PersonBrain : MonoBehaviour {
         switch (aiState) {
             case enAIState.idle: {
                 float time;
+                
+                this.rbody.velocity = Vector2.zero;
 
                 time = Random.Range(minIdleTime, maxIdleTime);
 
@@ -93,11 +109,25 @@ public class PersonBrain : MonoBehaviour {
 
                 time = Random.Range(minWalkTime, maxWalkTime);
 
-                // TODO Set speed
+                // Set speed
+                if (this.transform.position.x <= this.minHorPosition) {
+                    this.rbody.velocity = new Vector2(this.horizontalSpeed, 0.0f);
+                }
+                else if (this.transform.position.x >= this.maxHorPosition) {
+                    this.rbody.velocity = new Vector2(-this.horizontalSpeed, 0.0f);
+                }
+                else if (Random.Range(0, 1) == 0) {
+                    this.rbody.velocity = new Vector2(-this.horizontalSpeed, 0.0f);
+                }
+                else {
+                    this.rbody.velocity = new Vector2(this.horizontalSpeed, 0.0f);
+                }
 
                 Debug.Log("Walking for " + time.ToString() + "s");
                 yield return new WaitForSeconds(time);
                 Debug.Log("Exiting walk!");
+
+                this.rbody.velocity = Vector2.zero;
     
                 // Always go back to idle, after this
                 this.aiState = enAIState.idle;
