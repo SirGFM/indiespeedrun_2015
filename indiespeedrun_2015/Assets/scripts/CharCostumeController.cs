cb0ded976d6ddef576733cde1b5fc426b93b4aab
Ticolol <tico.amendola1@gmail.com>
2015-10-24 20:09:52 -0200

    Prontas as animações do personagem principal e sistema de troca de roupas

---
+++ indiespeedrun_2015/Assets/scripts/CharCostumeController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharCostumeController : MonoBehaviour {
+
+	public int level;
+
+	public Sprite[] corpos;
+	public Sprite[] biceps;
+	public Sprite[] antebraco;
+
+	// Use this for initialization
+	void Start () {
+		ChangeLevel(level);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ChangeLevel(int level){
+		foreach(SpriteRenderer s in GetComponentsInChildren<SpriteRenderer>()){
+			if(s.gameObject.name == "sprCorpo")
+				s.sprite = corpos[level];
+			if(s.gameObject.name == "sprBiceps")
+				s.sprite = biceps[level];
+			if(s.gameObject.name == "sprAntebraco")
+				s.sprite = antebraco[level];
+		}
+	}
+}
