7682d59bc8a94ea8b6150ed11ad2922d9f05091f
SirGFM <gfmandaji@hotmail.com>
2015-10-24 13:01:21 -0200

    Added coroutine for managing the AI

--- indiespeedrun_2015/Assets/scripts/PersonBrain.cs
+++ indiespeedrun_2015/Assets/scripts/PersonBrain.cs
@@ -30,13 +30,20 @@ public class PersonBrain : MonoBehaviour {
         max
     };
 
+    /** Minimum time the person will stand still if idle */
+    public float minIdleTime = 0.5f;
+    /** Maximum time the person will stand still if idle */
+    public float maxIdleTime = 0.5f;
     /** Current state of the person, relative to the player */
     public enState state;
     /** Which of the classes the instance represent */
     public enType type;
     /** This object's color */
     public Color color;
+    /** Current state of the AI */
     private enAIState aiState;
+    /** Probably unneeded, but avoids calling the coroutine if it's already running */
+    private bool isRunningAI;
 
 	// Use this for initialization
 	void Start () {
@@ -45,14 +52,50 @@ public class PersonBrain : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	    // TODO Set a
+        // TODO Set the animation according with the state
+
+        if (!isRunningAI) {
+            StartCoroutine("doAI");
+        }
 	}
 
+    private IEnumerator doAI() {
+        isRunningAI = true;
+        switch (aiState) {
+            case enAIState.idle: {
+                float time;
+
+                time = Random.Range(minIdleTime, maxIdleTime);
+
+                yield return new WaitForSeconds(time);
+
+                // TODO Select new state
+            } break;
+        }
+    }
+
+    /**
+     * Initialize the instance
+     * 
+     * @param type  The type of the person
+     * @param color The color of the person
+     */
     public void initInstance(enType type, Color color) {
+        // Set the instance's properties
         this.type = type;
         this.color = color;
+
+        // Always start on idle
+        this.aiState = enAIState.idle;
+        // Enable running the AI loop
+        this.isRunningAI = false;
+
+        // TODO Set this only on the shirt sprite
+        GetComponent<SpriteRenderer>().color = color;
+
         // TODO Randomize the position
         this.transform.position.Set(0, 0, 0);
+
         this.gameObject.SetActive(true);
     }
 
